6da6ff756d99ea2115abcd1e25821c79769c0e43
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-04-03 17:01:48 -0400

    added columns to category model

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,25 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    products: {
+      type: DataTypes.ARRAY,
+      references: {
+        model: 'products',
+        key: 'id',
+        unique: false
+
+      }
+    }
   },
   {
     sequelize,
