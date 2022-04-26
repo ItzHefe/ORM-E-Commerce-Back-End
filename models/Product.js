42f0da5b7b4be93575d13c84aca8b151c3e53edc
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-04-26 18:28:35 -0400

    fixed data models to successfully seed the DB

--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
     },
     stock: {
@@ -33,7 +33,7 @@ Product.init(
       }
     },
     category_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id',
